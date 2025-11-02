70683666bf4c2d0c9c07ff5c4474cd9381cd8c45
duckroll411 <JLU037@e.ntu.edu.sg>
2025-11-03 01:26:37 +0800

    game start and moving boundary

--- sketch.js
+++ sketch.js
@@ -6,31 +6,39 @@ let attackSize = 20;
 let vol;
 let attackColor = ["#0059ffff","#ffed24ff","#ff0e0eff"];
 let colorIndex = 0
+let gameStarted = false;
 
 
-const SPAWN_RATE = 90;
 /* ----------------- Assets ----------------- */
-let PlayerImg;
+
 
 function preload(){
   PlayerImg = loadImage("Assets/PlayerImg.png");
+  FonImg = loadImage("Assets/FonImg.png");
+  RedImg = loadImage("Assets/RedEnemyImg.png");
+  BlueImg = loadImage("Assets/BlueEnemyImg.png");
+  YellowImg = loadImage("Assets/YellowEnemyImg.png");
 }
 
+/* ----------------- Setup & Draw ----------------- */
 function setup() {
   imageMode(CENTER);
+  textAlign(CENTER, CENTER); 
   createCanvas(650, 650);
   background(102, 129, 124);
   fill(255);
-  ring = new attackRing(attackSize, width/2, height/2);
-  mic = new p5.AudioIn();
-  mic.start();
-  player = new Sprite(ring.x, ring.y, 50, STATIC);
+ 
 }
 
 function draw() {
-  background(102, 129, 124); 
+  background(102, 129, 124);
+  
+  if (!gameStarted) {
+    StartScreen();
+    return;
+  }
+  
   vol = mic.getLevel();
-
  if (keyIsDown(68)) { 
     ring.x += 4;
   }
@@ -44,11 +52,41 @@ function draw() {
     ring.y += 4;
   }
 
+  ring.x = constrain(ring.x, 0, width);
+  ring.y = constrain(ring.y, 0, height);
+
   ring.show();
+  image(FonImg, width/2, height/2, 50, 50,);
+
   console.log(ring.size);
-  player.rotateTowards(mouse, 0.1, 0);
+  //player.rotateTowards(mouse, 0.1, 0); (its not working :())
 }
 
+
+/* ----------------- StartScreen ----------------- */
+function StartScreen(){
+  fill(255);
+  textAlign(CENTER, CENTER); 
+  textSize(48);
+  text("SPACE key to Start", width/2, height/2);
+  textSize(20);
+  text("Hiss the cats away! WASD to move", width/2, height/2 + 50);
+  
+ 
+  image(PlayerImg, width/2, height/3 + 50, 50, 50);  
+}
+
+
+function keyPressed() {
+  if (keyCode === 32 && !gameStarted) {
+    gameStarted = true
+    ring = new attackRing(attackSize, width/2, height/2);
+    mic = new p5.AudioIn();
+    mic.start();
+  }
+}
+
+/* ----------------- Classes ----------------- */
 class attackRing {
   constructor(size, x, y) {
     this.size = size;
@@ -75,10 +113,11 @@ class attackRing {
     fill(attackColor[colorIndex])
     noStroke();
     ellipse(this.x, this.y, this.size, this.size);
+    
+   
     PlayerImg.resize(50, 0);
-    image(PlayerImg, this.x, this.y);      
+    image(PlayerImg, this.x, this.y);    
+     
   }
 }
-
 // add ui slider to adjust sensitivity (is at 5000 rn)
-
